d49435173893fb3a2fa3d071ebf9c9f842c5ae13
Alex D <alexdmspb@gmail.com>
2024-02-01 10:51:37 +0000

    feat: add periphery contracts

---
+++ packages/contracts/periphery/deploy/01_deploy_router02.ts
@@ -0,0 +1,34 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import packageJSON from "../package.json";
+
+const getUniswapV2FactoryAddress = async (chain: string, version: string) => {
+  const deployment = await import(
+    `@v60swap/core/deployments/${version}/${chain}/UniswapV2Factory.json`
+  );
+
+  return deployment.address;
+};
+
+const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployer } = await hre.getNamedAccounts();
+  let wethAddress = process.env.WETH_ADDRESS;
+
+  if (!hre.ethers.isAddress(wethAddress)) {
+    const weth = await hre.deployments.get("WETH9");
+    wethAddress = weth.address;
+  }
+
+  await hre.deployments.deploy("UniswapV2Router02", {
+    from: deployer,
+    args: [
+      await getUniswapV2FactoryAddress(hre.network.name, packageJSON.version),
+      wethAddress,
+    ],
+    log: true,
+  });
+};
+
+deploy.tags = ["core"];
+
+export default deploy;
---
+++ packages/contracts/periphery/hardhat.config.ts
@@ -0,0 +1,43 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy";
+import "hardhat-abi-exporter";
+import "dotenv/config";
+
+const packageJson = require("./package.json");
+
+const accounts = process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [];
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.6.6",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 999999,
+      },
+    },
+  },
+  networks: {
+    goerli: {
+      url: "https://goerli.drpc.org/",
+      accounts,
+      tags: ["weth", "core"],
+    },
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
+  abiExporter: {
+    path: "./dist",
+    runOnCompile: true,
+    clear: true,
+    flat: true,
+    pretty: false,
+  },
+  paths: {
+    deployments: `deployments/${packageJson.version}`,
+  },
+};
+
+export default config;
