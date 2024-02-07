59843d4dd795c8a25561630079280c280262b13e
georgyskr <gosha.skryuchenkov@gmail.com>
2024-02-07 15:46:35 +0200

    feat: add overrides

--- apps/web/src/features/liquidity-pair/read/useGetAmountOut.ts
+++ apps/web/src/features/liquidity-pair/read/useGetAmountOut.ts
@@ -1,5 +1,5 @@
 import { deployments, uniswapV2Router02Abi } from '@/global';
-import { useChainId, useReadContract } from 'wagmi';
+import { useChainId, useReadContract, UseReadContractParameters } from 'wagmi';
 
 type GetAmountOutArgs = SmartContractReadFunctionArgs<
   typeof uniswapV2Router02Abi,
@@ -7,22 +7,26 @@ type GetAmountOutArgs = SmartContractReadFunctionArgs<
 >;
 
 export type UseGetAmountOutParams = {
-  amountIn: GetAmountOutArgs['0'];
-  reserveIn: GetAmountOutArgs['1'];
-  reserveOut: GetAmountOutArgs['2'];
+  args: {
+    amountIn: GetAmountOutArgs['0'];
+    reserveIn: GetAmountOutArgs['1'];
+    reserveOut: GetAmountOutArgs['2'];
+  };
+  overrides: UseReadContractParameters;
 };
 
 export const useGetAmountOut = async ({
-  reserveIn,
-  reserveOut,
-  amountIn,
+  args,
+  overrides,
 }: UseGetAmountOutParams) => {
   const globalChainId = useChainId();
+  const { amountIn, reserveIn, reserveOut } = args;
 
   return useReadContract({
     abi: uniswapV2Router02Abi,
     address: deployments[globalChainId].UniswapV2Router02.address,
     functionName: 'getAmountOut',
     args: [amountIn, reserveIn, reserveOut],
+    ...overrides,
   });
 };
