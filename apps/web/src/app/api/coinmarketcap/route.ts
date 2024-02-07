73d9d8e385037723a5212c16e852ed93a8870867
georgyskr <gosha.skryuchenkov@gmail.com>
2024-02-07 15:34:32 +0200

    fix: pr review

--- apps/web/src/app/api/coinmarketcap/route.ts
+++ apps/web/src/app/api/coinmarketcap/route.ts
@@ -2,51 +2,46 @@ import { CoinMarketCapQuote } from '@/features/fiat-quote/types';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const symbol = searchParams.get('symbol');
+
+  if (!symbol || typeof symbol !== 'string') {
+    return NextResponse.json(
+      { error: 'Invalid token symbol' },
+      { status: 400 },
+    );
+  }
+
+  const apiUrl = `https://sandbox-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=${symbol}&convert=USD`;
+  const apiKey = '164b37a9-29fd-4c4c-9f9a-15b26a8370b7'; // TODO: replace CoinMarketCap with PROD key later
+
   try {
-    const { searchParams } = new URL(request.url);
-    const symbol = searchParams.get('symbol');
-
-    if (!symbol || typeof symbol !== 'string') {
-      return NextResponse.json(
-        { error: 'Invalid token symbol' },
-        { status: 400 },
-      );
-    }
-
-    const apiUrl = `https://sandbox-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=${symbol}&convert=USD`;
-    const apiKey = '164b37a9-29fd-4c4c-9f9a-15b26a8370b7'; // TODO: replace CoinMarketCap with PROD key later
-
     const response = await fetch(apiUrl, {
       method: 'GET',
       headers: {
         'X-CMC_PRO_API_KEY': apiKey,
         Accept: 'application/json',
       },
+      next: {
+        revalidate: 60,
+      },
     });
 
-    if (response.ok) {
-      const data: CoinMarketCapQuote = await response.json();
-      let quoteData = data.data[symbol];
-      let tokenPrice = null;
-      if (Array.isArray(quoteData)) {
-        tokenPrice = quoteData[0]?.quote.USD.price;
-      } else if (quoteData) {
-        tokenPrice = quoteData.quote.USD.price;
-      }
+    const data: CoinMarketCapQuote = await response.json();
+    let quoteData = data.data[symbol];
+    let tokenPrice = null;
 
-      return NextResponse.json(
-        {
-          symbol: symbol,
-          price: tokenPrice,
-        },
-        { status: 200 },
-      );
-    } else {
-      return NextResponse.json(
-        { error: 'Error at CoinMarketCap API' },
-        { status: response.status },
-      );
-    }
+    tokenPrice = Array.isArray(quoteData)
+      ? quoteData[0]?.quote.USD.price
+      : quoteData?.quote.USD.price;
+
+    return NextResponse.json(
+      {
+        symbol: symbol,
+        price: tokenPrice,
+      },
+      { status: 200 },
+    );
   } catch (error) {
     return NextResponse.json(
       { error: 'Internal Server Error' },
--- apps/web/src/features/fiat-quote/api/useFiatQuote.ts
+++ apps/web/src/features/fiat-quote/api/useFiatQuote.ts
@@ -1,31 +1,27 @@
 import { useEffect, useState } from 'react';
 
+const fetchFiatQuote = async (
+  tokenSymbol: string,
+  setFiatQuote: (value: number) => void,
+) => {
+  try {
+    const response = await fetch(`/api/coinmarketcap?symbol=${tokenSymbol}`);
+
+    const data = await response.json();
+
+    setFiatQuote(data.price);
+  } catch (error) {
+    console.error('Failed to fetch price with error: ', error);
+  }
+};
+
 export function useFiatQuote(tokenSymbol: string): number | null {
   const [fiatQuote, setFiatQuote] = useState<number | null>(null);
 
   useEffect(() => {
     if (!tokenSymbol) return;
-    const fetchFiatQuote = async () => {
-      try {
-        const response = await fetch(
-          `/api/coinmarketcap?symbol=${tokenSymbol}`,
-        );
 
-        if (response.ok) {
-          const data = await response.json();
-
-          if (data.price) {
-            setFiatQuote(data.price);
-          }
-        } else {
-          console.error('Failed to fetch fiat price for token: ', tokenSymbol);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchFiatQuote();
+    fetchFiatQuote(tokenSymbol, setFiatQuote);
   }, [tokenSymbol]);
 
   return fiatQuote;
