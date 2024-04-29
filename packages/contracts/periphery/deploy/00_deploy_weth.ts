df3f50c20966d5a9c1fe0e576bd12abc39a172d3
georgyskr <gosha.skryuchenkov@gmail.com>
2024-04-29 15:50:43 +0300

    chore: uncomment deploy part

--- packages/contracts/periphery/deploy/00_deploy_weth.ts
+++ packages/contracts/periphery/deploy/00_deploy_weth.ts
@@ -2,12 +2,12 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 
 const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  // const { deployer } = await hre.getNamedAccounts();
-  // await hre.deployments.deploy("WETH9", {
-  //   from: deployer,
-  //   args: [],
-  //   log: true,
-  // });
+  const { deployer } = await hre.getNamedAccounts();
+  await hre.deployments.deploy("WETH9", {
+    from: deployer,
+    args: [],
+    log: true,
+  });
 };
 
 deploy.tags = ["weth"];
