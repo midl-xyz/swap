6426be599ca5312da3436b19ef22697451ff7cfa
Valery Verenich <ver.valery99@gmail.com>
2024-07-29 12:07:00 +0200

    feat: implemented swap details block

--- apps/web/src/widgets/swap-form/ui/utils.ts
+++ apps/web/src/widgets/swap-form/ui/utils.ts
@@ -11,17 +11,13 @@ export const calculatePriceImpact = ({
   const denominator = reserveIn + amountIn;
   const amountOut = numerator / denominator;
 
-  // Начальная цена перед сделкой
   const initialPrice = reserveOut / reserveIn;
 
-  // Новые резервы после сделки
   const newReserveIn = reserveIn + amountIn;
   const newReserveOut = reserveOut - amountOut;
 
-  // Новая цена после сделки
   const newPrice = newReserveOut / newReserveIn;
 
-  // Процентное изменение цены
   const priceImpact = ((newPrice - initialPrice) / initialPrice) * 100;
   return priceImpact.toFixed(2) + '%';
 };
