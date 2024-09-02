918573dc26693f2cf37478724cccd525b53d01ea
Valery Verenich <ver.valery99@gmail.com>
2024-09-02 20:22:44 +0200

    feat: removed TODO

--- apps/web/src/global/config/tokenConfig.ts
+++ apps/web/src/global/config/tokenConfig.ts
@@ -6,7 +6,6 @@ import { GetChainIdReturnType } from 'wagmi/actions';
 
 type ChainId = GetChainIdReturnType<typeof wagmiConfig>;
 
-//TODO: add promtestnet WETHByChain
 export const WETHByChain: Record<ChainId, Address> = {
   [sepolia.id]: '0x1B2B2d20D0a7B34c12327EEc02bE7D476552f6Bf',
   [promTestnet.id]: '0x426A4d924C60fC0f6546b8ee2Ac0aECB9fbcaEF5',
