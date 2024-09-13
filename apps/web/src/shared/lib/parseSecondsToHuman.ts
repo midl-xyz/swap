9ee0cd69e17a752b074182758b4959bf51e25bf1
georgyskr <gosha.skryuchenkov@gmail.com>
2024-09-13 13:20:31 +0300

    chore: remove console.log

--- apps/web/src/shared/lib/parseSecondsToHuman.ts
+++ apps/web/src/shared/lib/parseSecondsToHuman.ts
@@ -3,7 +3,5 @@ export const parseSecondsToHuman = (seconds: number) => {
   const m = Math.floor((seconds % 3600) / 60);
   const s = Math.floor(seconds % 60);
 
-  console.log(h, m, s);
-
   return `${h > 9 ? h : `0${h}`}:${m > 9 ? m : `0${m}`}:${s > 9 ? s : `0${s}`}`;
 };
