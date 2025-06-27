828eb091f557ba0c3e177a0087f0cd1c4b8970a4
georgyskr <gosha.skryuchenkov@gmail.com>
2025-06-27 16:34:40 +0300

    fix: build error

--- apps/web/src/global/providers/Web3Provider.tsx
+++ apps/web/src/global/providers/Web3Provider.tsx
@@ -1,20 +1,12 @@
 'use client';
 import { LastUsedTokensProvider } from '@/features';
-import { config, wagmiConfig } from '@/global';
+import { config, queryClient, wagmiConfig } from '@/global';
 import { AddressPurpose } from '@midl-xyz/midl-js-core';
 import { WagmiMidlProvider } from '@midl-xyz/midl-js-executor-react';
 import { MidlProvider } from '@midl-xyz/midl-js-react';
 import { SatoshiKitProvider } from '@midl-xyz/satoshi-kit';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
-
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      experimental_prefetchInRender: true,
-    },
-  },
-});
 
 export const Web3Provider = ({ children }: { children: React.ReactNode }) => {
   return (
