656a15f36bcd43237cad23b9f79622f1e64b317f
georgyskr <gosha.skryuchenkov@gmail.com>
2025-07-29 14:41:56 +0300

    chore: update midl js

--- apps/web/src/global/providers/Web3Provider.tsx
+++ apps/web/src/global/providers/Web3Provider.tsx
@@ -5,7 +5,6 @@ import { WagmiMidlProvider } from '@midl-xyz/midl-js-executor-react';
 import { MidlProvider } from '@midl-xyz/midl-js-react';
 import { SatoshiKitProvider } from '@midl-xyz/satoshi-kit';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { Suspense } from 'react';
 
 export const Web3Provider = ({ children }: { children: React.ReactNode }) => {
   return (
