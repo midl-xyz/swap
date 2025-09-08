7bc363cdd3c0e9c9760fee5e87474266efc0ec02
georgyskr <gosha.skryuchenkov@gmail.com>
2025-09-08 13:15:08 +0300

    feat: update

--- apps/web/src/global/config/config.ts
+++ apps/web/src/global/config/config.ts
@@ -1,7 +1,9 @@
-import { MempoolSpaceProvider, regtest } from '@midl-xyz/midl-js-core';
+import { regtest } from '@midl-xyz/midl-js-core';
+import { MaestroSymphonyProvider } from '@midl-xyz/midl-js-core/providers/MempoolSpaceProvider';
 import { createMidlConfig } from '@midl-xyz/satoshi-kit';
 
 export const config = createMidlConfig({
   networks: [regtest],
   persist: true,
+  runesProvider: new MaestroSymphonyProvider(),
 });
