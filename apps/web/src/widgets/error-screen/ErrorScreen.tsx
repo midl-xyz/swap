4c16fda8fe7f7c489922a4b70392690a5e83ec2d
Valery Verenich <ver.valery99@gmail.com>
2025-09-15 07:56:49 +0200

    feat: add animated error screen and maintenance mode support
    - Introduced a new `ErrorScreen` component with SVG animations using `gsap`.
    - Implemented maintenance mode logic in `RootLayout`, dynamically displaying the `ErrorScreen` when enabled.
    - Added `ErrorBackground` component for animated visuals in the error state.
    - Updated `package.json` and `pnpm-lock.yaml` to include `gsap` dependency.
    - Enhanced layout and styling for improved responsiveness and user experience.

--- apps/web/src/widgets/error-screen/ErrorScreen.tsx
+++ apps/web/src/widgets/error-screen/ErrorScreen.tsx
@@ -1,8 +1,12 @@
 'use client';
 
-import { type FC } from 'react';
+import { Button } from '@/shared/ui/button';
+import { ErrorBackground } from '@/widgets/error-screen/ErrorBackground';
+import { gsap } from 'gsap';
+import Link from 'next/link';
+import { FC, useEffect, useRef } from 'react';
 import { css } from '~/styled-system/css';
-import { VStack } from '~/styled-system/jsx';
+import { VStack, Stack } from '~/styled-system/jsx';
 
 type Props = {
   name: string;
@@ -17,17 +21,53 @@ export const ErrorScreen: FC<Props> = ({
   buttonHref,
   buttonText,
 }) => {
+  const svgRef = useRef<SVGSVGElement>(null);
+
+  useEffect(() => {
+    const svg = svgRef.current;
+    if (!svg) return;
+
+    const paths = svg.querySelectorAll<SVGPathElement>('path');
+    for (let i = 0; i < paths.length; i++) {
+      const path = paths[i];
+      const length = path.getTotalLength();
+
+      path.setAttribute('stroke-dasharray', length.toString());
+      path.setAttribute('stroke-dashoffset', length.toString());
+
+      gsap.to(path, {
+        strokeDashoffset: 0,
+        duration: Math.random() * 2 + 1, // random between 1–3s
+        delay: Math.random() * 2, // random 0–2s
+        yoyo: true,
+        repeat: -1,
+        ease: 'sine.inOut',
+      });
+    }
+  }, []);
+
   return (
     <VStack
       className={css({
         w: '100vw',
-        h: '100%',
+        h: '100vh',
         bg: 'white',
         position: 'relative',
         alignItems: 'center',
         justifyContent: 'center',
       })}
     >
+      <ErrorBackground
+        ref={svgRef}
+        className={css({
+          w: '100%',
+          h: '100%',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+        })}
+      />
+
       <VStack
         className={css({
           gap: 15,
@@ -38,27 +78,36 @@ export const ErrorScreen: FC<Props> = ({
         })}
       >
         <VStack gap={2}>
-          <span
-            className={css({
-              fontWeight: 500,
-              fontSize: '120px',
-              letterSpacing: '0.2px',
-              fontVariationSettings: "'wdth' 110",
-              color: '#DC7520',
-            })}
+          <Stack
+            fontWeight={500}
+            fontSize={{
+              base: '40px',
+              md: '120px',
+            }}
+            wordBreak="break-all"
+            letterSpacing="0.2px"
+            fontVariationSettings="'wdth' 110"
+            color="#DC7520"
           >
             {name}
-          </span>
-          <span className={css({ textStyle: 'subtitle.primary.medium' })}>
+          </Stack>
+          <Stack textStyle="subtitle.primary.medium" textAlign="center">
             {description}
-          </span>
+          </Stack>
         </VStack>
 
-        {/* <Link href={buttonHref || "/"}>
-					<Button appearance="ghost" size="large">
-						{buttonText || "Back to main page"}
-					</Button>
-				</Link> */}
+        <Link href={buttonHref || '/'}>
+          <Button
+            appearance="primary"
+            size="large"
+            backgroundColor="#0000009b"
+            paddingX={5}
+            paddingY={2}
+            borderRadius="80px"
+          >
+            {buttonText || 'Back to main page'}
+          </Button>
+        </Link>
       </VStack>
     </VStack>
   );
