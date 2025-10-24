b6385beb58dcedfdd455915ca23f200fde1c8e14
annazhlb <annacher714@gmail.com>
2025-10-24 15:18:11 +0100

    feat: update pair

--- apps/web/src/widgets/pairs/ui/Pairs.tsx
+++ apps/web/src/widgets/pairs/ui/Pairs.tsx
@@ -11,7 +11,7 @@ const COMMUNITY_PAIRS = [
   ['WARTOK', 'BTC'],
   ['FIRSTDONUTTOK', 'BTC'],
   ['TRYBFAMILYTOK', 'BTC'],
-  ['NETOBLISSBITCOIN', 'BUSD'],
+  ['BUSD', 'NETOBLISSBITCO'],
 ];
 export const Pairs = () => {
   const tokens = [
